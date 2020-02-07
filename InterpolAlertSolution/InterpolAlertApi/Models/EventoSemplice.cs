dc963b61bdd4b0ce3ea182fc036d83bf498b2126
Francesco Zanardi <zanardifrancesco98@gmail.com>
2020-02-07 10:27:10 +0100

    modifica dall'inglese all'italiano i messaggi di errore all'interno dei Dtos e nei modelli

--- InterpolAlertSolution/InterpolAlertApi/Models/EventoSemplice.cs
+++ InterpolAlertSolution/InterpolAlertApi/Models/EventoSemplice.cs
@@ -13,10 +13,10 @@ namespace InterpolAlertApi.Models
         [Required]
         public string EventoSempliceNome { get; set; }
         [Required]
-        [Range(1, 5, ErrorMessage = "Gravita must be between 1 ad 5")]
+        [Range(1, 5, ErrorMessage = "Il valore della Gravita deve essere tra 1 e 5")]
         public int EventoSempliceGravita { get; set; }
         [Required]
-        [StringLength(200, MinimumLength = 20, ErrorMessage = "Note must be between 20 and 200 characters")]
+        [StringLength(200, MinimumLength = 20, ErrorMessage = "La nota deve contenere tra 20 e 200 caratteri")]
         public string EventoSempliceNote { get; set; }
         [Required]
         public DateTime EventoSempliceData { get; set; }
